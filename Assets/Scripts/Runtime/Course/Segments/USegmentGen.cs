6009e14858d1c409f09b3ca0b13d3002488a2cdd
ComradeVanti <kingvanti650@gmail.com>
2022-01-29 19:39:55 +0100

    Rework u segment
    Now has random sizes and offsets

--- Assets/Scripts/Runtime/Course/Segments/USegmentGen.cs
+++ Assets/Scripts/Runtime/Course/Segments/USegmentGen.cs
@@ -9,44 +9,53 @@ namespace Foxy.Flipside.Segments
     public class USegmentGen : SegmentGen
     {
 
-        [SerializeField] private int radius;
+        [SerializeField] private int minHeight;
+        [SerializeField] private int maxHeight;
+        [SerializeField] private int minWidth;
+        [SerializeField] private int maxWidth;
         
         
         public override CourseSegment Generate(Vector2Int startPos, Rng rng)
         {
             var dir = rng.CoinToss() ? 1 : -1;
-            
+            var height = rng.InRange(minHeight, maxHeight);
+            var width = rng.InRange(minWidth, maxWidth);
+            var rest = CourseSegment.Length - width;
+            var offset = rng.InRange(0, rest);
             
             IEnumerable<CourseTile> GenTiles()
             {
                 var pos = new Vector2Int(startPos.x, startPos.y);
 
-                for (var i = 0; i < 2; i++)
+                for (var _ = 0; _ < rest - offset; _++)
                 {
                     pos.x++;
                     yield return new CourseTile(pos);
                 }
                 
-                for (var i = 0; i < radius; i++)
+                for (var _ = 0; _ < height; _++)
                 {
                     pos.y += dir;
                     yield return new CourseTile(pos);
                 }
                 
-                for (var i = 0; i < CourseSegment.Length - 2; i++)
+                for (var _ = 0; _ < width; _++)
                 {
                     pos.x++;
                     yield return new CourseTile(pos);
                 }
                 
-                for (var i = 0; i < radius; i++)
+                for (var _ = 0; _ < height; _++)
                 {
                     pos.y -= dir;
                     yield return new CourseTile(pos);
                 }
                 
-                pos.x++;
-                yield return new CourseTile(pos);
+                for (var _ = 0; _ < offset; _++)
+                {
+                    pos.x++;
+                    yield return new CourseTile(pos);
+                }
             }
 
             return new CourseSegment(GenTiles().ToArray());
