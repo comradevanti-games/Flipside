9aefec69c0d36a3d00b41158c431491b20400dc5
ComradeVanti <kingvanti650@gmail.com>
2022-01-29 15:13:24 +0100

    Add logic to get random item in array

---
+++ Assets/Scripts/Runtime/ArrayExt.cs
@@ -0,0 +1,11 @@
+﻿namespace Foxy.Flipside.Runtime
+{
+
+    public static class ArrayExt
+    {
+
+        public static T RandomItem<T>(this T[] items, Rng rng) => items[rng.InRange(0, items.Length - 1)];
+
+    }
+
+}
