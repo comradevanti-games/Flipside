efbc1e0dbde18faf0c12fb302b0bed3a68883d31
lhalbritter-tgm <lhalbritter@student.tgm.ac.at>
2022-01-29 18:11:44 +0100

    Enemy Basic Behaviour

--- Assets/Scripts/Runtime/BaseEnemyBehaviour.cs
+++ Assets/Scripts/Runtime/BaseEnemyBehaviour.cs
@@ -1,21 +1,102 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Foxy.Flipside
 {
     public class BaseEnemyBehaviour : MonoBehaviour
     {
+        [SerializeField] private float velocity = 10f;
+        [SerializeField] private float trackRadius = 1.0f;
+
+        private Rigidbody enemyRb;
+        private Transform playerTransform;
+        
+        public EnemyState currentState = EnemyState.SEARCHING;
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            enemyRb = GetComponent<Rigidbody>();
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            Vector3 target = transform.position;
+            Vector3 direction;
+            switch (currentState)
+            {
+                case EnemyState.SEARCHING:
+                    RaycastHit[] hits = Physics.SphereCastAll(transform.position, trackRadius, Vector3.back, Mathf.Infinity);
+                    Debug.Log(hits.Length);
+                    if (hits.Length > 0)
+                    {
+                        foreach (RaycastHit hit in hits)
+                        {
+                            Debug.Log(hit.collider.tag);
+                            if (hit.collider.CompareTag("Player"))
+                            {
+                                playerTransform = hit.transform;
+                                currentState = EnemyState.TARGETING;
+                                break;
+                            }
+                        }
+
+                        if (playerTransform) break;
+                    }
+
+                    target = ChooseRandomPoint();
+                    break;
+                
+                case EnemyState.TARGETING:
+                    target = playerTransform.position;
+                    if (Vector3.Distance(transform.position, playerTransform.position) < 2.0f)
+                        currentState = EnemyState.ATTACKING;
+                    break;
+                case EnemyState.ATTACKING:
+                    target = playerTransform.position;
+                    break;
+                default:
+                    currentState = EnemyState.SEARCHING;
+                    break;
+            }
+            
+            direction = (target - transform.position).normalized;
+            enemyRb.AddForce(direction * velocity, ForceMode.Acceleration);
+        }
+
+        Vector3 ChooseRandomPoint()
+        {
+            Vector3 curPos = transform.position;
+            float x = Random.Range(curPos.x - trackRadius, curPos.x + trackRadius);
+            float y = curPos.y;
+            float z = Random.Range(curPos.z - trackRadius, curPos.z + trackRadius);
+            return new Vector3(x, y, z);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.collider.CompareTag("Player"))
+            {
+                Debug.Log("Player dies");
+            }
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(transform.position, trackRadius);
+        }
+#endif
+
+        public enum EnemyState
+        {
+            SEARCHING,
+            TARGETING,
+            ATTACKING
         }
     }
 }
