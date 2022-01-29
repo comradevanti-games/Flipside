392e736793cfe03b56f802f386149bace2ef4438
ComradeVanti <kingvanti650@gmail.com>
2022-01-29 15:53:45 +0100

    Add basic course generation

--- Assets/Scripts/Runtime/Course/SegmentBuilder.cs
+++ Assets/Scripts/Runtime/Course/SegmentBuilder.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Foxy.Flipside
 {
@@ -7,7 +6,20 @@ namespace Foxy.Flipside
     public class SegmentBuilder : MonoBehaviour
     {
 
-        public void Build(CourseSegment segment) => throw new NotImplementedException();
+        [SerializeField] private Transform mapParent;
+        [SerializeField] private float courseHeight;
+        [SerializeField] private GameObject tilePrefab;
+
+        public void Build(CourseSegment segment)
+        {
+            foreach (var tile in segment.Tiles) BuildTile(tile);
+        }
+
+        private void BuildTile(CourseTile tile)
+        {
+            var pos = new Vector3(tile.Position.x, courseHeight, tile.Position.y);
+            Instantiate(tilePrefab, pos, Quaternion.identity, mapParent);
+        }
 
     }
 
