3cf87badca8a2d50b2f4f27998011a0b8e597308
ComradeVanti <kingvanti650@gmail.com>
2022-01-29 15:31:40 +0100

    Add basic course-manager

---
+++ Assets/Scripts/Runtime/Course/SegmentBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Foxy.Flipside
+{
+
+    public class SegmentBuilder : MonoBehaviour
+    {
+
+        public void Build(CourseSegment segment) => throw new NotImplementedException();
+
+    }
+
+}
