b6ac5519851b31b1f74d127053367afaa9392771
lhalbritter-tgm <lhalbritter@student.tgm.ac.at>
2022-01-29 17:38:06 +0100

    moved Player Scripts into corresponding folder, created Enemy Prefab

---
+++ Assets/Scripts/Runtime/BaseEnemyBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Foxy.Flipside
+{
+    public class BaseEnemyBehaviour : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Player/FaceSideAbilities.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Foxy.Flipside
+{
+    public class FaceSideAbilities : BaseSideAbilities
+    {
+
+        [SerializeField] [Range(0.0f, 1.0f)] private float gravityScale = 0.1f;
+        [SerializeField] private float gravity = -9.81f;
+
+        private Vector3 applyGravity;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            base.Init();
+            sideUpAbilities.Add(KeyCode.Mouse0, SideUpBlow);
+            sideDownAbilities.Add(KeyCode.Mouse0, SideDownBlow);
+            applyGravity = gravity * gravityScale * Vector3.up;
+        }
+
+        void SideUpBlow()
+        {
+            // todo: block enemies
+            Debug.Log("Face: Block Enemy");
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                playerBehaviour.PlayerRb.useGravity = true;
+                abilityActive = false;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (!abilityActive) return;
+
+            playerBehaviour.PlayerRb.AddForce(applyGravity, ForceMode.Acceleration);
+        }
+
+        void SideDownBlow()
+        {
+            // todo: Hover logic
+            Debug.Log("HOVER");
+            playerBehaviour.PlayerRb.useGravity = false;
+            abilityActive = true;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            playerBehaviour.PlayerRb.useGravity = true;
+            abilityActive = false;
+        }
+    }
+}
