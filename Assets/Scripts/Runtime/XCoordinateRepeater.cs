28b91713c937de3d04490cfcd96ca224556c73cf
ComradeVanti <kingvanti650@gmail.com>
2022-01-30 11:49:50 +0100

    Add logic to repeat background elements

---
+++ Assets/Scripts/Runtime/XCoordinateRepeater.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace Foxy.Flipside
+{
+
+    public class XCoordinateRepeater : MonoBehaviour
+    {
+
+        [SerializeField] private float repeatX;
+        [SerializeField] private float startX;
+
+
+        private Vector3 LocalPos
+        {
+            get => transform.localPosition;
+            set => transform.localPosition = value;
+        }
+
+        private float LocalX
+        {
+            get => LocalPos.x;
+            set => LocalPos = new Vector3(value, LocalPos.y, LocalPos.z);
+        }
+
+        private bool ShouldRepeat =>
+            LocalX <= repeatX;
+
+
+        private void Update()
+        {
+            if (ShouldRepeat)
+                Repeat();
+        }
+
+        private void Repeat() =>
+            LocalX = startX;
+
+    }
+
+}
